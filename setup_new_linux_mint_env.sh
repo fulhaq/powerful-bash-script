6c092364fac33f9f8d3f10e83e8045b68e8d785b
Farman Ulhaq <fulhaq@napa.com>
2019-05-08 12:20:49 -0400

    fixed installing kubectl on windows subsystem

--- setup_new_linux_mint_env.sh
+++ setup_new_linux_mint_env.sh
@@ -119,9 +119,10 @@ runAsRoot apt-get install google-cloud-sdk -y
 #Ref:  https://kubernetes.io/docs/tasks/tools/install-kubectl/
 #Ref:
 echo 'Installing Kubectl'
-runAsRoot apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+runAsRoot apt-get update
+runAsRoot apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 runAsRoot apt-get update
 runAsRoot apt-get install -y kubectl
 
