75ad0c35eb51516a96f7ca68050d57f26e9b446a
Farman Ulhaq <fulhaq@napa.com>
2019-05-22 11:51:48 -0400

    added tx

--- clone-repos.sh
+++ clone-repos.sh
@@ -7,11 +7,19 @@ do
     git clone git@bitbucket.org:napagroupnyc/$i.git ~/Documents/napa/infrastructure/$i/
 done
 
-#TRIDENT
+#TRIDENT-NAPA
 declare -a arr=("trident-front-end" "trident-back-end")
 for i in "${arr[@]}"
 do
    echo "$i"
     git clone git@bitbucket.org:napagroupnyc/$i.git ~/Documents/napa/trident/$i/
+done
+
+#TRIDENT
+declare -a arr=("k8-desired-state" "k8-prod-desired-state" "devops")
+for i in "${arr[@]}"
+do
+   echo "$i"
+    git clone git@bitbucket.org:tridentx/$i.git ~/Documents/tridentx/devops/$i/
 done
 
